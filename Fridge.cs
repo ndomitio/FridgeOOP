0fb53d0c5c73703d56e7d9a713403eec3f8f0307
NicholasDomitio <nicholas.domitio@gmail.com>
2018-01-31 15:20:24 -0500

    end of class session

--- Fridge.cs
+++ Fridge.cs
@@ -81,7 +81,55 @@ namespace FridgeOOP
             }
         }
 
+        public string ChangeBulbs()
+        {
+            if (bulbsWork == false)
+            {
+                bulbsWork = true;
+                return "The bulbs have been changed";
+            }
+            else
+            {
+                return "The bulbs don't have to be changed";
+            }
 
+        }
+
+        public void RemoveFood(int foodRemoved)
+        {
+            foodAmount -= foodRemoved;
+        }
+
+        public string CheckSupplies()
+        {
+            if (foodAmount >= 80)
+            {
+                return "fully stocked";
+            }
+            else if (foodAmount > 40)
+            {
+                return "getting low";
+            }
+            else
+            {
+                return "time to go grocery shopping";
+            }
+        }
+        public string Clean(bool isDirty)
+        {
+            if (isDirty)
+            {
+                isClean = true;
+                return "I cleaned the fridge";
+            }
+            else
+            {
+                return "The fridge is already clean.";
+            }
+            
+        }
+
+     
 
     }
 }
