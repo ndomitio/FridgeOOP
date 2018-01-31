64251726a39716ecf2e766822b5108e793e91041
NicholasDomitio <nicholas.domitio@gmail.com>
2018-01-31 14:44:26 -0500

    first

---
+++ Fridge.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FridgeOOP
+{
+
+    public class Fridge
+    {
+        //fields =="baby bears"
+
+        private bool isClean;//I dont want the variable to able to be manipulated outside of this...camel case
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+
+        //Properties===="mama bear" get/set.......get allows us to see the value...set allows us to assign the value...pascal
+
+        public bool IsClean
+        {
+            get { return this.isClean; }
+            set { this.isClean = value; }
+        }
+
+        public string DispenserType  //we don't want user to be able to change the value
+        {
+            get { return this.dispenserType; }
+            //set {return this.dispenserType = "basic water";}   in this instance every fridge instantiated would have this string 
+        }
+        public int FoodAmount
+
+        {
+            get { return this.foodAmount; }
+            set { this.foodAmount = value; }
+        }
+        public bool BulbsWork
+        {
+            get { return this.bulbsWork; }
+            set { this.bulbsWork = value; }
+        }
+
+        //Constructors...always need a default, but EVERYTIME a fridge is instantiated we need a dispenser type
+        public Fridge()
+        {
+            this.dispenserType = "basic water dispenser";     //since we didn't do a set in the property, we wanted to make sure that EVERY fridge still has a dispenser type.
+        }
+
+        public Fridge(string dispenserType)
+        {
+            this.dispenserType = dispenserType; ///field on left from top of page, parameter on right is the one in parantheses
+        }
+
+        public Fridge(bool isClean, string dispenserType, int foodAmount, bool bulbsWork)
+        {
+            this.isClean = isClean;
+            this.dispenserType = dispenserType;
+            this.foodAmount = foodAmount;
+            this.bulbsWork = bulbsWork;
+        }
+
+        //Methods()  focus on the MVP!
+        //i need a way to dispense water
+        //i need a way to change bulbs
+        //i need a way to clean fridge
+        //i need a way to check food supplies
+
+        public void DispenseWater()
+        {
+            if (dispenserType == "basic water dispenser")
+            {
+                Console.WriteLine("Here's some lukewarm water!");
+            }
+            else if (dispenserType == "deluxe dispenser")
+            {
+                Console.WriteLine("Enjoy your perfect ice water.");
+            }
+            else
+            {
+                Console.WriteLine("What's gonna come out? It's a surprise!");
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FridgeOOP
+{
+    public class Program
+    {
+    }
+}
